045af189754e3038012238c827d111c61ec96ed7
manish021124 <maneeshbalami@gmail.com>
2022-08-10 18:00:31 +0545

    update

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const btn = document.getElementById("btn");
-let input;
+// let input;
 
 btn.addEventListener("submit", addItem);
 
 function addItem(){
-    getInput();
-    displayInput();
-}
-
-function getInput(){
-    input = document.getElementById('entry').elements.value;
-}
-
-function displayInput(){
+    // getInput();
+    // displayInput();
+    let input = document.getElementById('entry').elements.value;
     document.getElementById("list").innerHTML = input;
+
+
 }
+
+// function getInput(){
+//     input = document.getElementById('entry').elements.value;
+// }
+
+// function displayInput(){
+//     document.getElementById("list").innerHTML = input;
+// }

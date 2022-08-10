29ef55014b7ba4f02a04a633318007cd2d46adb1
manish021124 <maneeshbalami@gmail.com>
2022-08-10 16:23:59 +0545

    update

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const btn = document.getElementById("btn");
 let input;
 
-btn.addEventListener("click", addItem);
+btn.addEventListener("submit", addItem);
 
 function addItem(){
     getInput();
     displayInput();
-    console.log("hey");
 }
 
 function getInput(){
-    input = entry.elements[0].value;
+    input = document.getElementById('entry').elements.value;
 }
 
 function displayInput(){
